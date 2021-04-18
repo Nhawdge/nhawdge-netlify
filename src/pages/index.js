ed44f2bce78d6e7af88049efb88e2af9db740857
John Pavek <JohnQPavek@gmail.com>
2021-04-18 16:39:13 -0500

    Fixing URL Linking issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,9 @@ export default function IndexPage() {
               title
               type
             }
+            fields {
+              slug
+            }
           }
         }
       }
@@ -29,21 +32,17 @@ export default function IndexPage() {
       <SEO title="Home" />
       <h1>Nhawdge.net</h1>
       <p>
-        Hi and welcome to my personal, professional, and everything else blog. I'm keeping it simple, and blogging about things I'm interested in, and things I've done.
+        Hi and welcome to my personal, professional, and everything else blog.
+        I'm keeping it simple, and blogging about things I'm interested in, and
+        things I've done.
       </p>
       <ul>
         {blogs.map(x => (
           <li>
-            <Link
-              to={`/blog/${x.node.frontmatter.title
-                .toLowerCase()
-                .split(" ")
-                .join("-")}`}
-            >
-              {x.node.frontmatter.title}
-            </Link>
+            <Link to={x.node.fields.slug}>{x.node.frontmatter.title}</Link>
             <span className={"tag " + x.node.frontmatter.type.toLowerCase()}>
-              {x.node.frontmatter.type}</span>
+              {x.node.frontmatter.type}
+            </span>
           </li>
         ))}
       </ul>
